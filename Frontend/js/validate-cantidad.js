b06b4b19dc76aa8691572c99679993f5271a2a32
WildRavenZ <franzafer2001@hotmail.com>
2025-07-07 13:48:16 -0600

    Delete Frontend directory

--- Frontend/js/validate-cantidad.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const cantidadInput = document.getElementById("cantidad");
-
-  cantidadInput.addEventListener("input", function () {
-    this.value = this.value.replace(/[^1-9\d]/g, '');
-    this.value = this.value.replace(/^0+/, '');
-  });
-});
